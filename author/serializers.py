e65db561ee2a3bf700c4328f8e2852b92418fef9
Olena Pyslar <pislar33@gmail.com>
2026-06-05 21:54:52 +0300

    Solution Fixed

--- author/serializers.py
+++ author/serializers.py
@@ -16,7 +16,8 @@ class AuthorSerializer(serializers.ModelSerializer):
 
     def update(self, instance, validated_data):
         instance.first_name = validated_data.get(
-            "first_name", instance.first_name)
+            "first_name",
+            instance.first_name)
         instance.last_name = validated_data.get(
             "last_name", instance.last_name)
         instance.pseudonym = validated_data.get(
--- author/urls.py
+++ author/urls.py
@@ -11,3 +11,4 @@ app_name = "author"
 urlpatterns = [
     path("", include(router.urls)),
 ]
+

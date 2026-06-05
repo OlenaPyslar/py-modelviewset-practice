8cd730fe54309fe4343eb9718acb18f28c2aeed5
Olena Pyslar <pislar33@gmail.com>
2026-06-05 21:47:05 +0300

    Solution

--- author/urls.py
+++ author/urls.py
@@ -1,3 +1,15 @@
-# Create your urls here
+from django.urls import path, include
+from rest_framework import routers
 
-urlpatterns = []
+from author.views import AuthorViewSet
+
+router = routers.DefaultRouter()
+router.register("author", AuthorViewSet)
+
+app_name = "author"
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
+
+
